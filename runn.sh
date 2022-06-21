f30ae907e1a51cc3b78f7a356d70169309489be6
Zer0Pwn3d <103738597+Zer0Pwn3d@users.noreply.github.com>
2022-06-22 02:28:50 +0700

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -1,1 +1,1 @@
-wget -O logo.txt https://pastebin.com/raw/kMt9BBP4; clear; while true;do cat logo.txt; echo -e; read -p "root@agus-hackpath:~$ "  cmd; $1$cmd;done
+wget -O logo.txt https://pastebin.com/raw/kMt9BBP4; clear; while true;do cat logo.txt; echo -e; read -p "root@agus-hackpath:~$ "  cmd; $1$cmd;done; clear

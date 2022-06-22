481263e03c3983ca1e433fd7463787df88209f13
Zer0Pwn3d <103738597+Zer0Pwn3d@users.noreply.github.com>
2022-06-22 18:14:14 +0700

    Update runn.sh

--- runn.sh
+++ runn.sh
@@ -1,1 +1,2 @@
 wget -O logo.txt https://pastebin.com/raw/kMt9BBP4; clear; while true;do cat logo.txt; echo -e; uname -a; date; read -p "root@agus-hackpath:~$ "  cmd; $1$cmd;done; clear
+bash install
